4fd2918f808eb449838b50c44a6d1a7bcb0b1c27
KITAGAWA Masaki <kitagawa.stilo@gmail.com>
2023-10-08 23:41:31 +0900

    update reading memo, add markdown to html

---
+++ sandbox/zenbuiri_python/src/zenbuiri_python/chapter08/convert_md_to_html.py
@@ -0,0 +1,16 @@
+import locale
+import os
+import webbrowser
+import markdown2
+
+md_file = 'sample/chapter08/sample1.md'
+html_file ='tmp/sample1.html'
+html_enc = locale.getpreferredencoding()
+
+with open(md_file, 'r', encoding='utf-8') as md, open(html_file, 'w', encoding=html_enc) as html:
+  md_format = md.read()
+  html_format = markdown2.markdown(md_format)
+  html.write(html_format)
+  current_dir = os.getcwd()
+  uri = f'file://{current_dir}/{html_file}'
+  webbrowser.open_new_tab(uri)
